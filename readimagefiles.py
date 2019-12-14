458ea4fcee04b1c2c519a072910e9cf6017f5b2c
junhaogu <57818048+junhaogu@users.noreply.github.com>
2019-12-15 04:53:00 +0800

    Update readimagefiles.py

--- readimagefiles.py
+++ readimagefiles.py
@@ -13,6 +13,7 @@ import os
 from PIL import Image
 from matplotlib import pyplot as plt
 import torchvision.transforms as transforms
+from random import shuffle
 import numpy as np
 def read_single_cell_image(image_data_path, mask_data_path):
   data_path=os.path.join(image_data_path, '*.tif')
@@ -36,7 +37,7 @@ def read_single_cell_image(image_data_path, mask_data_path):
       sample_data=torch.cat((sample_data,temp[1:,:,:,:]),0)
       temp = torch.zeros([1,3,256,512]).type(torch.FloatTensor)
   sample_data=sample_data[1:,:,:,:]
-  
+
 
   mask = torch.zeros([1,1,256,512]).type(torch.float)
   temp = torch.zeros([1,1,256,512]).type(torch.float)
@@ -56,4 +57,10 @@ def read_single_cell_image(image_data_path, mask_data_path):
   
   plt.imshow(mask[0][0,:,:])
   plt.imshow(sample_data[0][0,:,:])
+    indexlist=[]
+  for i in range (sample_data.shape[0]):
+      indexlist.append(i)
+  shuffle(indexlist)
+  sample_data=sample_data[indexlist,:,:,:]
+  mask=mask[indexlist,:,:,:]
   return sample_data, mask
